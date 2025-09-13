eeb3082eaa666c2e210a745fe2fa0d88cd335f86
Sarwesh0910 <sarweshudhaya@gmail.com>
2025-09-14 00:53:46 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI/CD pipeline!");
+    }
+
+    public String greet() {
+        return "Hello from Jenkins CI/CD pipeline!";
+    }
+}
